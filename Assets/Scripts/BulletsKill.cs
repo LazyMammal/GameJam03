fd07b01c795563d4928a7b6822d568423ba15eaa
Dennis Carlstrom <lazy@lazymammal.com>
2017-08-05 19:20:38 -0400

    HeroBullet works on Enemy_Invader

---
+++ Assets/Scripts/BulletsKill.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletsKill : MonoBehaviour
+{
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Bullet"))
+		{
+			Destroy(other.transform.parent.gameObject);
+			// TODO: spawn explosion
+			// TODO: notify scoring system
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/TimeKills.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeKills : MonoBehaviour
+{
+	public float deathDelay = 3.0f;
+	private float deathTime;
+
+	void Start()
+	{
+		deathTime = Time.time + deathDelay;
+	}
+
+	void Update()
+	{
+		if (Time.time > deathTime)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
