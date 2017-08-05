265f69df521ae803eb7fbda1b37971614a23b117
Dennis Carlstrom <lazy@lazymammal.com>
2017-08-05 18:41:45 -0400

    bullet movement test

---
+++ Assets/Scripts/ShootBullets.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullets : MonoBehaviour
+{
+	public Transform[] Bullet_Types;
+	public Transform BulletSpawnPoint; // Y-forward orientation
+	public float fireRate = 10f; // per second
+	private float nextTime = 0.0f;
+
+	void Update()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextTime)
+		{
+			nextTime = Time.time + 1f / fireRate;
+			ShootBullet();
+		}
+	}
+
+	void ShootBullet()
+	{
+		// spawn bullet
+		if (Bullet_Types.Length > 0)
+		{
+			GameObject item = Bullet_Types[Random.Range(0, Bullet_Types.Length)].gameObject;
+			GameObject go = (GameObject)Instantiate(item, BulletSpawnPoint.transform.position, BulletSpawnPoint.transform.rotation);
+		}
+	}
+}
