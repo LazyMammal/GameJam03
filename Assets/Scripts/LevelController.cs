df8b66223e5edb15727850c822cb7b04848d6267
Dennis Carlstrom <lazy@lazymammal.com>
2017-08-06 16:36:51 -0400

    Demo3, transitions for testing

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -6,7 +6,8 @@ public class LevelController : MonoBehaviour
 {
 	private GameController gameCtrl;
 	public float maxLevelDuration = 10f; // time kill level
-	public bool usePlayerShip = true;
+	public float minDuration = 1f;
+	public bool isTransition = false;
 	private float levelStartTime;
 	private bool levelActive = false;
 
@@ -17,7 +18,7 @@ public class LevelController : MonoBehaviour
 
 		levelStartTime = Time.time;
 		levelActive = true;
-		gameCtrl.SetPlayerShip(usePlayerShip);
+		gameCtrl.SetPlayerShip(!isTransition);
 	}
 	public void DoLevelDone()
 	{
@@ -31,9 +32,22 @@ public class LevelController : MonoBehaviour
 			levelActive = false;
 			DoLevelDone();
 		}
+
+		if (levelActive && isTransition && Input.anyKey && (Time.time - GetLevelStartTime()) > minDuration)
+		{
+			DoLevelDone();
+		}
+
 	}
 
-	public bool isLevelActive() {
+	public bool isLevelActive()
+	{
 		return levelActive;
 	}
+
+	public float GetLevelStartTime()
+	{
+		return levelStartTime;
+	}
+
 }
