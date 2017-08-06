b7ced532c208433eb8b4a6a32682298de134ab8e
Rob Waite <fooish@hotmail.com>
2017-08-06 13:46:30 -0400

    main scene update
    removed unneeded scenes, updated gui layers

---
+++ Assets/Scripts/SpriteScaleEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteScaleEffect : MonoBehaviour {
+
+    public float spriteScale;
+    public float scaleMultiplier = 0.15f;
+
+    
+    public GameObject spritename;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            
+     //transform.localScale = new Vector3(Mathf.PingPong(Time.time, 1) + scaleMultiplier, Mathf.PingPong(Time.time, 1) + scaleMultiplier, 0);
+         
+
+
+
+
+    }
+}
