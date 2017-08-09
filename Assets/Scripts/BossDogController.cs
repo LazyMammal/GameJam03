b062882d4051fcda06deaa6b4ee95161f72c23e0
Rob Waite <fooish@hotmail.com>
2017-08-09 15:43:50 -0400

    updates

---
+++ Assets/Scripts/BossDogController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDogController : MonoBehaviour {
+
+    private Animator bossdogAnimate;
+    private GameObject bossDogGameObject;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Bullet")
+        {
+
+            GetComponent<Animator>().SetBool("BossDogHit", true);
+
+            Invoke("SetBoolBack", 2);
+            
+
+        }
+    }
+    private void SetBoolBack()
+    {
+        GetComponent<Animator>().SetBool("BossDogHit", false);
+    }
+}
