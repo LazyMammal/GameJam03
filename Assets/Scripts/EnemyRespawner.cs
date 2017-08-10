23a5f5e95267f97e7f50c471225f399b9395a723
Dennis Carlstrom <lazy@lazymammal.com>
2017-08-10 05:58:28 -0400

    full version Level 7: Big Boss

---
+++ Assets/Scripts/EnemyRespawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRespawner : MonoBehaviour
+{
+	public Transform[] Enemy_Types;
+	public float delay = 2f;
+	public bool randomize = true;
+	private float nextTime = 0f;
+	private int enemyIndex = 0;
+
+	void Update()
+	{
+		// spawn enemies
+		if (Enemy_Types.Length > 0 && Time.time > nextTime)
+		{
+			nextTime = Time.time + delay;
+			GameObject item;
+			if (randomize)
+			{
+				item = Enemy_Types[Random.Range(0, Enemy_Types.Length)].gameObject;
+			}
+			else
+			{
+				item = Enemy_Types[(enemyIndex++) % Enemy_Types.Length].gameObject;
+			}
+			GameObject go = (GameObject)Instantiate(item, transform.position, transform.rotation);
+		}
+	}
+}
---
+++ Assets/Scripts/ExplodeOnImpact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeOnImpact : MonoBehaviour
+{
+	public Animator anim;
+
+	void Start() {
+		anim = GetComponentInChildren<Animator>();
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			anim.SetBool("Explode", true);
+			// TODO: notify scoring system
+			//Destroy(gameObject);
+		}
+	}
+}
