1f403554c5495855cc28db7e3d848af704934f05
Dennis Carlstrom <lazy@lazymammal.com>
2017-08-10 07:22:08 -0400

    bullets also explode enemies

--- Assets/Scripts/ExplodeOnImpact.cs
+++ Assets/Scripts/ExplodeOnImpact.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class ExplodeOnImpact : MonoBehaviour
 {
 	public GameObject explosion;
-
-	void Start() {
-		//if( explosion == null) {
-			//explosion = GameObject.Find("Explosion");
-			explosion = (GameObject) Resources.Load("Explosion");
-		//}
+	void Start()
+	{
+		if (explosion == null)
+		{
+			explosion = (GameObject)Resources.Load("Explosion");
+		}
 	}
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.gameObject.CompareTag("Player") && explosion != null)
+		if (other.gameObject.CompareTag("Player"))
 		{
 			// TODO: notify scoring system
 			GameObject go = (GameObject)Instantiate(explosion, transform.position, Quaternion.identity);
