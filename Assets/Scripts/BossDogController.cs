f4602f0d01f57045d98ebf6372efb2bb77c84775
Dennis Carlstrom <lazy@lazymammal.com>
2017-08-11 03:03:50 -0400

    add Rob's SFX: start screen, bullets, boss death

--- Assets/Scripts/BossDogController.cs
+++ Assets/Scripts/BossDogController.cs
@@ -2,47 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossDogController : MonoBehaviour {
+public class BossDogController : MonoBehaviour
+{
+	public int bossDogHealth = 255;
+	private Animator bossdogAnimate;
+	private AudioSource aSource;
+	public GameController gameCtrl;
+	void Start()
+	{
+		if (gameCtrl == null)
+		{
+			GameObject go = GameObject.Find("GameController");
+			gameCtrl = (GameController)go.GetComponent(typeof(GameController));
+		}
+		aSource = GetComponent<AudioSource>();
+	}
+	void Update()
+	{
+		GetComponent<SpriteRenderer>().color = new Color32(255, (byte)bossDogHealth, (byte)bossDogHealth, 255);
+	}
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Bullet")
+		{
+			if (bossDogHealth > 0)
+			{
+				GetComponent<Animator>().SetBool("BossDogHit", true);
+				Invoke("SetBoolBack", 0.5f);
+    			bossDogHealth--;
+			}
 
-    public byte bossDogHealth;
+			if (bossDogHealth <= 0)
+			{
+                GetComponent<Animator>().SetBool("BossDogHit", true);
+				aSource.Play();
+				foreach (Transform t in transform)
+				{
+					if (t.gameObject.GetComponent(typeof(EnemyRespawner)))
+					{
+						t.gameObject.SetActive(false);
+					}
+				}
+				StartCoroutine(DeathWait());
+			}
+		}
+	}
+	private void SetBoolBack()
+	{
+		GetComponent<Animator>().SetBool("BossDogHit", false);
+	}
 
-    private Animator bossdogAnimate;
+	public IEnumerator DeathWait()
+	{
+		yield return new WaitForSeconds(1.25f);
+        gameCtrl.EnemyKilled(gameObject.GetInstanceID());
+		Destroy(gameObject);
+	}
 
-
-    // Use this for initialization
-    void Start () {
-
-        bossDogHealth = 255;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-
-        GetComponent<SpriteRenderer>().color = new Color32(255, bossDogHealth, bossDogHealth, 255);
-
-        //Debug.Log(bossDogHealth);
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.tag == "Bullet")
-        {
-            GetComponent<Animator>().SetBool("BossDogHit", true);
-
-            Invoke("SetBoolBack", 0.5f);
-
-            bossDogHealth--;
-
-            if (bossDogHealth == 0)
-            {
-                Destroy(gameObject);
-            }
-
-        }
-    }
-    private void SetBoolBack()
-    {
-        GetComponent<Animator>().SetBool("BossDogHit", false);
-    }
 }
