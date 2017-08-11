2a4302f74ea57c296d673276840b262402137862
Dennis Carlstrom <lazy@lazymammal.com>
2017-08-11 00:14:31 -0400

    Bonus Stage: Tetris

---
+++ Assets/Scripts/OffsetAndReturn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetAndReturn : MonoBehaviour
+{
+	public float duration = 1.5f;
+	public Vector3 minOffset = new Vector3(0f, 1f, 0f);
+	public Vector3 maxOffset = new Vector3(0f, 2f, 0f);
+	private Vector3 savedLocalPosition, savedRandPosition;
+	private float startTime;
+
+	void Start()
+	{
+		savedLocalPosition = transform.localPosition;
+		Vector3 offsetPos = new Vector3(
+			Random.Range(minOffset.x, maxOffset.x),
+			Random.Range(minOffset.y, maxOffset.y),
+			Random.Range(minOffset.z, maxOffset.z)
+		);
+
+		transform.localPosition += offsetPos;
+		savedRandPosition = transform.localPosition;
+
+		startTime = Time.time;
+	}
+	void Update()
+	{
+		transform.localPosition = Vector3.Lerp(savedRandPosition, savedLocalPosition, (Time.time - startTime) / duration);
+	}
+}
--- Assets/Scripts/ZigZagDescend.cs
+++ Assets/Scripts/ZigZagDescend.cs
@@ -6,6 +6,7 @@ public class ZigZagDescend : MonoBehaviour
 {
 	// setup
 	public float speed = 1f; // speed in X-axis
+	public float startDelay = 0f;
 	public float verticalSpacing = 1.5f; // step size
 	public Vector2 horizontalLimits = new Vector2(-5f, 5f); // min,max
 	public Vector2 verticalLimits = new Vector2(-10f, 10f); // min,max
@@ -14,7 +15,7 @@ public class ZigZagDescend : MonoBehaviour
 	bool descending = false;
 	private Vector2 velocity;
 	private Vector3 waypoint;
-
+	private float startTime;
 	private LevelController lvlCtrl;
 
 	void Start()
@@ -24,41 +25,46 @@ public class ZigZagDescend : MonoBehaviour
 		descending = false;
 		velocity = new Vector2(-speed, -speed);
 		waypoint = transform.position;
+
+		startTime = Time.time;
 	}
 
 	void Update()
 	{
-		var pos = transform.position;
-
-		// check if descent is complete
-		if (descending && Mathf.Abs(waypoint.y - pos.y) >= verticalSpacing)
+		if (Time.time > startTime + startDelay)
 		{
-			descending = false;
-			velocity.x *= -1f;
-			waypoint = pos;
-		}
+			var pos = transform.position;
 
-		// check if zig/zag is complete
-		else if (!descending && (pos.x < horizontalLimits.x || pos.x > horizontalLimits.y))
-		{
-			descending = true;
-			waypoint = pos;
-		}
+			// check if descent is complete
+			if (descending && Mathf.Abs(waypoint.y - pos.y) >= verticalSpacing)
+			{
+				descending = false;
+				velocity.x *= -1f;
+				waypoint = pos;
+			}
 
-		// move group along only one axis
-		Vector3 newPos = pos;
-		pos.x = Mathf.Clamp(pos.x, horizontalLimits.x, horizontalLimits.y);
-		pos.y = Mathf.Clamp(pos.y, verticalLimits.x, verticalLimits.y);
+			// check if zig/zag is complete
+			else if (!descending && (pos.x < horizontalLimits.x || pos.x > horizontalLimits.y))
+			{
+				descending = true;
+				waypoint = pos;
+			}
 
-		if (descending)
-		{
-			newPos = new Vector3(pos.x, pos.y + velocity.y * Time.deltaTime, pos.z);
-		}
-		else
-		{
-			newPos = new Vector3(pos.x + velocity.x * Time.deltaTime, pos.y, pos.z);
-		}
+			// move group along only one axis
+			Vector3 newPos = pos;
+			pos.x = Mathf.Clamp(pos.x, horizontalLimits.x, horizontalLimits.y);
+			pos.y = Mathf.Clamp(pos.y, verticalLimits.x, verticalLimits.y);
 
-		transform.position = newPos;
+			if (descending)
+			{
+				newPos = new Vector3(pos.x, pos.y + velocity.y * Time.deltaTime, pos.z);
+			}
+			else
+			{
+				newPos = new Vector3(pos.x + velocity.x * Time.deltaTime, pos.y, pos.z);
+			}
+
+			transform.position = newPos;
+		}
 	}
 }
